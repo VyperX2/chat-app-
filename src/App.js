7e06adfde56e0331a31948ae368542a246481811
VyperX2 <mateenahmed.qatar@gmail.com>
2023-08-23 15:45:09 +0300

    added authentication and connected to firestore

--- src/App.js
+++ src/App.js
@@ -9,11 +9,62 @@ import { auth, db, provider } from "./firebase";
 
 function App() {
 
+  const [user] = useAuthState(auth)
+  const messagesRef = db.collection('messages')
+  const query = messagesRef.orderBy('createdAt').limit(25)
+  const [messages] = useCollectionData(query)
+
+  function signInWithGoogle(){
+    auth.signInWithPopup(provider)
+  }
+
+  function ChatRoom(){
+
+    function ChatMessage({ key , message}){
+      return <h5> {message.text} </h5>
+    }
+
+    function sendMessages(e){
+      e.preventDefault();
+
+    }
+
+
+    return(
+      <div>
+        <header>
+          <button onClick={() => auth.signOut()}>Sign Out</button>
+        </header>
+        <section>
+          <form onSubmit={sendMessages}>
+            <input type="text" />
+            <button type="submit" >🚀</button>
+          </form>
+          {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+
+        </section>
+      </div>
+    )
+  }
+
+  function SignIn(){
+    return(
+      <button onClick={signInWithGoogle}>
+        Sign In!
+      </button>
+    )
+  }
 
 
   return (
     <div className="App">
-      
+      <header>
+        
+      </header>
+
+      <section>
+        {user? <ChatRoom /> : <SignIn /> }
+      </section>
     </div>
   );
 }
