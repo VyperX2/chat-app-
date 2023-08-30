770dd33235a8ceecd3666e5f38ab52a43d22f0a9
Mateen <136918338+VyperX2@users.noreply.github.com>
2023-08-30 14:00:36 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,8 @@ import 'firebase/compat/firestore';
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB3dAhN0vzY3EEAbWOXcMUs5hdI0Hqg37A",
-  authDomain: "practiceapp-18bd7.firebaseapp.com",
-  projectId: "practiceapp-18bd7",
-  storageBucket: "practiceapp-18bd7.appspot.com",
-  messagingSenderId: "629015991551",
-  appId: "1:629015991551:web:7dd31a706167858c24a8ca",
-  measurementId: "G-DKD4BHR8D5"
+  //Your Config
+  
 };
 
 
